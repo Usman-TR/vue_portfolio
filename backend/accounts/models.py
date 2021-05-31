dba52649f92b0d3106e65ffa1d45007ce90dd465
= <i.ezerbaev97@gmail.com>
2021-05-31 18:54:24 +0300

    add models CustomUser

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -6,6 +6,6 @@ class CustomUser(AbstractUser):
     middle_name = models.CharField(max_length=100)
     about_me = models.CharField(max_length=500)
     rating = models.FloatField(default=3.0)
-    university = models.ForeignKey('core.University', on_delete=models.SET_NULL) # not delete
+    university = models.ForeignKey('core.University', on_delete=models.SET_NULL, null=True, blank=True) # not delete
     profile = models.ManyToManyField('core.Profile')
     books = models.ManyToManyField('core.Book')
