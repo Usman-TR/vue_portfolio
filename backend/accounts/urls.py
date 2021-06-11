525c8e5836f6fcc32a766b3df25ad03acddeac5d
= <i.ezerbaev97@gmail.com>
2021-06-11 23:28:24 +0300

    добавил вьюху из рест фреймворка для отображения кокретного юзера

---
+++ backend/accounts/urls.py
@@ -0,0 +1,11 @@
+from django.urls import  path, include
+
+from .views import UserDetailView
+
+
+urlpatterns = [
+    path('api/v1/', include('rest_auth.urls')),
+    path('api/v1/registration/', include('rest_auth.registration.urls')),
+    path('<pk>/', UserDetailView.as_view())
+    #path('users/<str:username>/books/', get_user_books, name='get_user_books')
+]
