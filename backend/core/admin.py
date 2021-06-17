e4b65c1874668fa254d81015499dad7cf087577f
= <i.ezerbaev97@gmail.com>
2021-06-17 21:12:28 +0300

    sending email | change models

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 from django.db.models.deletion import PROTECT
-from .models import Book, University, Profile
+from .models import Book, University, Profile, Achievement, Ratings
 # Register your models here.
 
 admin.site.register(Book)
 admin.site.register(University)
 admin.site.register(Profile)
+admin.site.register(Achievement)
+admin.site.register(Ratings)
