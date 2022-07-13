bbe2134aa195728b1287cc9a84f712d829c5ef8b
Khamzat <bahaev.hamzat95@gmail.com>
2022-07-13 14:52:58 +0300

    add page and component for statistics

---
+++ frontend/src/components/StaticItem.vue
@@ -0,0 +1,113 @@
+<template>
+  <div class="static-item">
+    <div class="static-item__title">{{staticItem.title}}</div>
+    <div class="static-item-content">
+      <div class="progressbar">
+        <div class="progressbar__value" :style="{width: staticItem.progress + '%'}"></div>
+      </div>
+      <div class="static-item__label">{{staticItem.status}}</div>
+      <div class="static-item__label">{{staticItem.pages}}</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    staticItem: {
+      type: Object,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.static-item {
+  padding: 8px 15px 10px;
+  border-radius: 8px;
+  height: 80px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  margin-bottom: 15px;
+}
+.static-item__title {
+  font-size: 14px;
+  line-height: 20px;
+  letter-spacing: 0.25px;
+  margin-right: auto;
+}
+.static-item-content {
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: space-between;
+}
+.static-item__label {
+  letter-spacing: 0.4px;
+  font-size: 12px;
+  line-height: 16px;
+}
+.progressbar {
+  width: 100%;
+  position: relative;
+  height: 5px;
+  border-radius: 12px;
+  margin-bottom: 7px;
+}
+.progressbar__value {
+  position: absolute;
+  height: 5px;
+  left: 0;
+  border-radius: 12px;
+}
+.static-item:nth-child(1n) {
+  border: 1px solid #dacafc;
+  background: #F5F0FF;
+  & .static-item__title {
+    color: #835ed2;
+  }
+  & .progressbar {
+    background-color: #dacafc;
+    &__value {
+    background-color: #835ed2;
+    }
+  }
+  & .static-item__label {
+    color: #b195eb;
+  }
+}
+.static-item:nth-child(2n) {
+  background: #FFF4E6;
+  border: 1px solid #F2DEC2;
+  & .static-item__title {
+    color: #FF9500;
+  }
+  & .progressbar {
+    background-color: #F9DCB4;
+    &__value {
+    background-color: #FF9500;
+    }
+  }
+  & .static-item__label {
+    color: #F4BE71;
+  }
+}
+
+.static-item:nth-child(3n) {
+  background: #E7FFED;
+  border: 1px solid #5ECC8D;
+  & .static-item__title {
+    color: #219653;
+  }
+  & .progressbar {
+    background-color: #AFF0C5;
+    &__value {
+    background-color: #219653;
+    }
+  }
+  & .static-item__label {
+    color: #5ECC8D;
+  }
+}
+</style>
---
+++ frontend/src/views/Static.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="static">
+    <h5 class="static__title">Статистика</h5>
+    <ul>
+      <StaticItem
+        v-for="item in staticItems"
+        :staticItem="item"
+        :key="item.id"
+      />
+    </ul>
+  </div>
+</template>
+
+<script>
+import StaticItem from '../components/StaticItem.vue'
+export default {
+  components: { StaticItem },
+  data () {
+    return {
+      staticItems: [
+        {
+          id: 1,
+          title: 'Информационные технологии',
+          status: 'Прочитано',
+          progress: 25,
+          pages: '54/300'
+        },
+        {
+          id: 2,
+          title: 'Машинное обучение',
+          status: 'Читаю',
+          progress: 48,
+          pages: '222/224'
+        },
+        {
+          id: 3,
+          title: 'Промышленные технологии',
+          status: 'Прочитано',
+          progress: 100,
+          pages: '59/152'
+        },
+        {
+          id: 4,
+          title: 'Информационные технологии',
+          status: 'Прочитано',
+          progress: 90,
+          pages: '54/300'
+        },
+        {
+          id: 5,
+          title: 'Машинное обучение',
+          status: 'Читаю',
+          progress: 12,
+          pages: '222/224'
+        },
+        {
+          id: 6,
+          title: 'Промышленные технологии',
+          status: 'Прочитано',
+          progress: 33,
+          pages: '59/152'
+        },
+        {
+          id: 51,
+          title: 'Машинное обучение',
+          status: 'Читаю',
+          progress: 65,
+          pages: '222/224'
+        },
+        {
+          id: 62,
+          title: 'Промышленные технологии',
+          status: 'Прочитано',
+          progress: 75,
+          pages: '59/152'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  padding: 0;
+}
+
+.static {
+  padding: 1rem;
+}
+
+.static__title {
+  text-align: start;
+  font-size: 24px;
+  line-height: 28px;
+  font-weight: 400;
+  margin-bottom: 1rem;
+}
+
+.btn {
+  color: #835ed2;
+}
+</style>
